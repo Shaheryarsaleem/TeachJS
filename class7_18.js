9174a05208b9119df4bdf3ca6ba539456069e149
Muhammad Shaheryar saleem <34899382+Shaheryarsaleem@users.noreply.github.com>
2019-12-26 20:31:24 +0500

    Add files via upload

---
+++ class7_18.js
@@ -0,0 +1,42 @@
+console.log("more on Events");
+
+let btn = document.getElementById('btn');
+
+
+// btn.addEventListener('click', fun1);
+// btn.addEventListener('mousedown', fun2);
+
+// btn.addEventListener('dblclick', fun3);
+
+
+// function fun1(e){
+//     console.log("Thanks", e);
+//     e.preventDefault();    
+// }
+
+// function fun2(e){
+//     console.log("mouse down event fire", e);
+//     e.preventDefault();    
+// }
+
+
+// function fun3(e){
+//     console.log("doubleclick fire", e);
+//     e.preventDefault();    
+// }
+
+
+
+// document.querySelector('.this').addEventListener('mouseenter', function(){
+//     console.log('you enter');
+// })
+
+// document.querySelector('.this').addEventListener('mouseleave', function(){
+//     console.log('you exit');
+// })
+
+// document.querySelector('.container').addEventListener('mousemove', function(e){
+//     console.log(e.offsetX, e.offsetY);
+//     document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 154)`;
+//     console.log('move mouse');
+// })
